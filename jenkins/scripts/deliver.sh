50d88465a1f0e7f7d3cf97073734368cc18505f8
Melih Yildiz <mzyil12@gmail.com>
2020-12-31 09:07:30 +0300

    move credentials to a kubernetes secret object

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
+
 NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 
 GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 JAR_FILE_NAME="$NAME-$VERSION.jar"
 JAR_FILE_PATH="$PWD/target/$JAR_FILE_NAME"
-PASSWORD="Mzyil."
 REPO="docker.tartismaliyiz.com"
-USERNAME="mzyil"
 
 IMAGE_TAG="$REPO/$NAME:$VERSION-$GIT_COMMIT_SHA"
 docker build -t $IMAGE_TAG --build-arg JAR_FILE_NAME=$JAR_FILE_NAME .
-echo $PASSWORD | docker login --username $USERNAME --password-stdin $REPO
+echo $DOCKER_REPO_PASSWORD | docker login --username $DOCKER_REPO_USERNAME --password-stdin $REPO
 docker push $IMAGE_TAG
